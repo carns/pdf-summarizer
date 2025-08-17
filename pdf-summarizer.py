00eea022dac7edd28dbe9b75a15eb5e7395c87aa
Phil Carns <carns@mcs.anl.gov>
2025-08-16 21:31:22 -0400

    pythonic iteration

--- pdf-summarizer.py
+++ pdf-summarizer.py
@@ -61,8 +61,7 @@ def extract_text_from_pdf(pdf_path: str) -> str:
     text = ""
     with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
+        for page in reader.pages:
             text += page.extract_text()
     return text
 
